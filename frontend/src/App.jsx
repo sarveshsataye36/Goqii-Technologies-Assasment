f8fc1ce0182155b16d59d1b37b604ee49bd7137b
Sarvesh Sataye <sarvesh.sataye@neosoftmail.com>
2024-06-03 17:05:02 +0530

    Basic route setup for application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Navbar from './components/Navbar/Navbar';
-import { Footer } from './components/Navbar/Footer/Footer';
+import { Footer } from './components/Footer/Footer';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Add } from './Pages/Home/Add';
+import { List } from './Pages/List/List';
 
 function App() {
 
   return (
-    <>
+    <Router>
     <Navbar/>
+      <Routes>
+        <Route path="/" element={<Add/>} />    
+        <Route path="/list" element={<List/>} />
+      </Routes>
     <Footer />
-    </>
+    </Router>
   )
 }
 
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import reactLogo from '../../assets/react.svg'
+import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark  bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"><img src={reactLogo} classNameName="logo curd" alt="curd logo" />  CURD APP</a>
+                <Link className="navbar-brand" to="/"><img src={reactLogo} classNameName="logo curd" alt="curd logo" />  CURD APP</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Add</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Add</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">List</a>
+                            <Link className="nav-link" to="/list">List</Link>
                         </li>
                     </ul>
                 </div>

a31da8ddf01dc874b5def55e517fabe6b2223c00
Sarvesh Sataye <sarvesh.sataye@neosoftmail.com>
2024-06-03 18:10:33 +0530

    Update Footer UI. Display content center

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <div className="container-fluid footerDown">
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <div className="col-md-4 d-flex align-items-center">
+            <div className="col-md-12 d-flex align-items-center justify-content-center">
             <span className="mb-3 mb-md-0 text-body-secondary">© 2024 CRUD APP, Inc</span>
             </div>
         </footer>

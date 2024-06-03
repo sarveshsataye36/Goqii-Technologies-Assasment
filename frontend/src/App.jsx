7715f168741fb20c6ed9265f76bcbcd770fb8358
Sarvesh Sataye <sarvesh.sataye@neosoftmail.com>
2024-06-03 16:42:19 +0530

    Footer Created for the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Navbar from './components/Navbar/Navbar';
+import { Footer } from './components/Navbar/Footer/Footer';
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <Footer />
     </>
   )
 }
